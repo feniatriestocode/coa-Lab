fcc22ed77216e744732bb3aac7914fc9f178085b
chriskara <karagiannisxr03@gmail.com>
2022-10-20 15:47:38 +0300

    Better solution

--- lab3/f.asm
+++ lab3/f.asm
@@ -1,8 +1,8 @@
 .text
 .globl main
 main:
-	li $a0, 3
-	li $a1, 5
+	li $a0, 8
+	li $a1, 9
 	jal f
 
 	li $v0, 10
@@ -12,32 +12,25 @@ f:
 	beqz $a0, m
 	beqz $a1, n
 
-	addi $sp, $sp, -12
-	sw $ra, 8($sp)
-	sw $a0, 4($sp)
-	sw $a1, ($sp)
+	addi $sp, $sp, -4
+	sw $ra, ($sp)
 
 	addi $a0, $a0, -1
 	jal f
+	addi $a0, $a0, 1
 
-	lw $a0, 4($sp)
-	lw $a1, ($sp)
-	addi $sp, $sp, 4
-	sw $v0, ($sp)
 	addi $a1, $a1, -1
 	jal f
+	addi $a1, $a1, 1
 
-	lw $t0, ($sp)
-	add $v0, $v0, $t0
-
-	lw $ra, 4($sp)
-	addi $sp, $sp, 8
+	lw $ra, ($sp)
+	addi $sp, $sp, 4
 	jr $ra
 
 	m:
-		move $v0, $a1
+		add $v0, $v0, $a1
 		jr $ra
 
 	n:
-		move $v0, $a0
+		add $v0, $v0, $a0
 		jr $ra
