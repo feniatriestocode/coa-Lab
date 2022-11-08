4cd8559a7e615d1e6dd5a474ed8e9c5a4bc8eaaa
chriskara <karagiannisxr03@gmail.com>
2022-11-08 20:25:30 +0200

    Needs only testbench

--- lab4/library.v
+++ lab4/library.v
@@ -10,9 +10,9 @@
 
 module ALU (out, zero, inA, inB, op);
   parameter N = 32;
-  output    [N-1:0] out;
+  output signed [N-1:0] out;
   output    zero;
-  input     [N-1:0] inA, inB;
+  input signed [N-1:0] inA, inB;
   input     [3:0] op;
 
   reg       [N-1:0] out;
@@ -34,9 +34,9 @@ parameter AND = 4'B0000,
     ADD:
       out = inA + inB;
     SUB:
-      out = inA + ~(inB);
+      out = inA + (~inB + 1);
     SLT:
-      out = ((inA < inB) ? 1:0);
+      out = (inA + (~inB + 1) >= 0) ? 0 : 1;
     NOR:
       out =  ~(inA | inB);
 
@@ -57,11 +57,11 @@ endmodule
 module RegFile (clock, reset, raA, raB, wa, wen, wd, rdA, rdB);
 	input clock, reset, wen;
 	input [4:0] raA, raB, wa;
-	input  [31:0] wd;
-	output [31:0] rdA, rdB;
+	input signed [31:0] wd;
+	output signed [31:0] rdA, rdB;
 
 	integer i;
-	reg [31:0] data [32];
+	reg signed [31:0] data [32];
 
 	assign rdA = data[raA];
 	assign rdB = data[raB];
--- lab4/testbench.v
+++ lab4/testbench.v
@@ -7,8 +7,8 @@ module cpu_tb;
 reg       clock, reset;    // Clock and reset signals
 reg   [4:0] raA, raB, wa;
 reg         wen;
-wire   [31:0] wd;
-wire  [31:0] rdA, rdB;
+wire signed  [31:0] wd;
+wire signed [31:0] rdA, rdB;
 integer i;
 
 reg [3:0] op;
@@ -34,11 +34,11 @@ initial begin  // Ta statements apo ayto to begin mexri to "end" einai seiriaka
 
   // Now apply some inputs. 
   // You SHOULD EXTEND this part of the code with extra inputs
-   wen = 0; raA = 32'h1; raB = 32'h13; op = 4'h2;
+   wen = 0; raA = 32'h0; raB = 32'h13; op = 4'h6;
 #(2*`clock_period)
    wa = 32'h1E; wen = 1;
 #(2*`clock_period)
-   wen = 0; raA = 32'hA; raB = 32'h4; op = 4'h6; 
+   wen = 0; raA = 32'h1E; raB = 32'h4; op = 4'h7; 
 #(2*`clock_period)
    wa = 32'hE; wen = 1'b1;
 #(2*`clock_period)
