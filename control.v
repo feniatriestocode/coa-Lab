93bcaa56e9340af60894f5351fb9f8a61b1e33fd
Fenia <96126980+feniatriestocode@users.noreply.github.com>
2022-12-07 12:58:48 +0200

    **STILL DEBUGGING - UPLOADED FOR BACKUP**

---
+++ control.v
@@ -0,0 +1,166 @@
+`include "constants.h"
+`timescale 1ns/1ps
+
+/************** Main control in ID pipe stage *************/
+module control_main(output reg RegDst, output reg Branch, output reg jump, output reg MemRead, output reg MemWrite, output reg MemToReg, output reg ALUSrc, output reg RegWrite, output reg BneEn, output reg [1:0] ALUcntrl, input [5:0] opcode);
+	always @(opcode)
+		begin
+			case (opcode)
+				`R_FORMAT:
+					begin
+						RegDst 		= 1'b1;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b0;
+						Branch 		= 1'b0;
+						ALUcntrl 	= 2'b10;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b1;
+						jump 		= 1'b0;
+					end
+				`LW:
+					begin
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b1;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b1;
+						ALUSrc 		= 1'b1;
+						Branch 		= 1'b0;
+						ALUcntrl 	= 2'b00;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b1;
+						jump 		= 1'b0;
+					end
+				`SW:
+					begin 
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b1;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b1;
+						Branch 		= 1'b0;
+						ALUcntrl 	= 2'b00;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b0;
+						jump 		= 1'b0;
+					end
+				`BEQ:
+					begin 
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b0;
+						Branch 		= 1'b1;
+						ALUcntrl 	= 2'b01;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b0;
+						jump 		= 1'b0;
+					end
+				`BNE:
+					begin 
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b0;
+						Branch 		= 1'b1;
+						ALUcntrl 	= 2'b01;
+						BneEn		= 1'b1;
+						RegWrite    = 1'b0;
+						jump 		= 1'b0;
+					end
+				`ADDI:
+					begin 
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b1;
+						Branch 		= 1'b0;
+						ALUcntrl 	= 2'b00;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b1;
+						jump 		= 1'b0;
+					end
+				`J: begin
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b0;
+						Branch 		= 1'b0;
+						ALUcntrl 	= 2'b00;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b0;
+						jump 		= 1'b1;
+					end
+				default:
+					begin
+						RegDst 		= 1'b0;
+						MemRead 	= 1'b0;
+						MemWrite 	= 1'b0;
+						MemToReg 	= 1'b0;
+						ALUSrc 		= 1'b0;
+						Branch 		= 1'b0;
+						ALUcntrl 	= 2'b00;
+						BneEn		= 1'b0;
+						RegWrite    = 1'b0;
+						jump 		= 1'b0;
+					end
+			endcase
+		end
+endmodule
+
+/**************** Module for Bypass Detection in EX pipe stage goes here *********/
+module forwarding_unit (output [1:0] forwardA, output [1:0] forwardB, output forwardC, input [4:0] IDEX_RegisterRs, input [4:0] IDEX_RegisterRt, input [4:0] EXMEM_RegisterRd, input [4:0] MEMWB_RegisterRd , input EXMEM_RegWrite, input MEMWB_RegWrite, input EXMEM_MemWrite);
+		assign forwardA = (EXMEM_RegWrite && (EXMEM_RegisterRd != 0) && (IDEX_RegisterRs == EXMEM_RegisterRd)) ? 2 :
+			(MEMWB_RegWrite && (MEMWB_RegisterRd != 0) && (MEMWB_RegisterRd == IDEX_RegisterRs) && ((IDEX_RegisterRs != EXMEM_RegisterRd) || ~EXMEM_RegWrite)) ? 1 : 0;
+
+		assign forwardB = (EXMEM_RegWrite && (EXMEM_RegisterRd != 0) && (IDEX_RegisterRt == EXMEM_RegisterRd)) ? 2 :
+			(MEMWB_RegWrite && (MEMWB_RegisterRd != 0) && (MEMWB_RegisterRd == IDEX_RegisterRt) && ((IDEX_RegisterRt != EXMEM_RegisterRd) || ~EXMEM_RegWrite)) ? 1 : 0;
+
+		assign forwardC = (EXMEM_MemWrite && (EXMEM_RegisterRd == MEMWB_RegisterRd)) ? 1 : 0;
+endmodule
+
+/**************** Module for Stall Detection in ID pipe stage goes here *********/
+module hazard_unit(output PCwrite, output IFID_write, output BranchStall, output NOPen, input IDEX_MemRead, 
+					input [4:0] IDEX_RegisterRt, input [4:0] IFID_RegisterRs, 
+					input [4:0] IFID_RegisterRt, input [5:0] opcode);
+					
+	assign NOPen = (IDEX_MemRead && ((IDEX_RegisterRt == IFID_RegisterRs) || (IFID_RegisterRt == IDEX_RegisterRt))) ? 0 : 1;
+	assign BranchStall = ( (opcode == `BEQ) || (opcode == `BNE) || (opcode == `J) ) ? 0 : 1;
+	assign PCwrite = NOPen;
+	assign IFID_write = NOPen;
+endmodule
+					
+/************** control for ALU control in EX pipe stage *************/
+module control_alu(output reg [3:0] ALUOp, input [1:0] ALUcntrl, input [5:0] func);
+	always @(ALUcntrl or func)
+		begin
+			case (ALUcntrl)
+				2'b10:
+					begin
+						case (func)
+							6'b100000: ALUOp = 4'b0010;// add
+							6'b100010: ALUOp = 4'b0110;// sub
+							6'b100100: ALUOp = 4'b0000;// and
+							6'b100101: ALUOp = 4'b0001;// or
+							6'b100111: ALUOp = 4'b1100;// nor
+							6'b101010: ALUOp = 4'b0111;// slt
+							6'b000000: ALUOp = 4'b1111;// sll
+							6'b000100: ALUOp = 4'b1000;// sllv
+							6'b100110: ALUOp = 4'b1010;// xor
+							default: ALUOp = 4'b0000;
+						endcase 
+					end
+				2'b00:
+					ALUOp  = 4'b0010; // add
+				2'b01:
+					ALUOp = 4'b0110; // sub
+				default:
+					ALUOp = 4'b0000;
+			endcase
+		end
+endmodule
---
+++ cpu.v
@@ -0,0 +1,227 @@
+`include "control.v"
+`include "library.v"
+`timescale 1ns/1ps
+
+module cpu(input clock, input reset);
+	reg [31:0] PC;
+	reg [31:0] IFID_PCplus4;
+	reg [31:0] IFID_instr;
+
+	reg [31:0] IDEX_rdA, IDEX_rdB, IDEX_signExtend, IDEX_jumpaddr;
+	reg [4:0]  IDEX_instr_rt, IDEX_instr_rs, IDEX_instr_rd, IDEX_Shamt;
+	reg        IDEX_RegDst, IDEX_ALUSrc;
+	reg [1:0]  IDEX_ALUcntrl;
+	reg        IDEX_Branch, IDEX_MemRead, IDEX_MemWrite, IDEX_BneEn, IDEX_jump;
+	reg        IDEX_MemToReg, IDEX_RegWrite;
+
+	reg [4:0]  EXMEM_RegWriteAddr, EXMEM_instr_rd;
+	reg [31:0] EXMEM_ALUOut;
+	reg        EXMEM_Zero, EXMEM_PCSrc;
+	reg [31:0] EXMEM_MemWriteData, EXMEM_branchaddr;
+	reg        EXMEM_Branch, EXMEM_MemRead, EXMEM_MemWrite, EXMEM_RegWrite, EXMEM_MemToReg, EXMEM_BneEn;
+
+	reg [31:0] MEMWB_DMemOut;
+	reg [4:0]  MEMWB_RegWriteAddr, MEMWB_instr_rd;
+	reg [31:0] MEMWB_ALUOut;
+	reg        MEMWB_MemToReg, MEMWB_RegWrite;
+
+	wire [31:0] instr, ALUInA, ALUInB, ALUOut, rdA, rdB, signExtend, DMemOut, wRegData, datatowrite, rdBetween; 
+	wire Zero, RegDst, MemRead, MemWrite, MemToReg, ALUSrc, Branch, jump, BneEn, forwardC, PCSrc, RegWrite, PCEn, BranchStall, IFID_En, bubble_idex;
+	wire [5:0] opcode, func;
+	wire [4:0] instr_rs, instr_rt, instr_rd, RegWriteAddr, shamt;
+	wire [3:0] ALUOp;
+	wire [1:0] ALUcntrl, forwardA, forwardB;
+	wire [15:0] imm;
+	wire [31:0] branchaddr, jumpaddr;
+
+	/***************** Instruction Fetch Unit (IF) ****************/
+	always @(posedge clock or negedge reset)
+		begin 
+			if (~reset)
+				PC = -4;
+			else if (PCEn && PCSrc) begin
+				PC = EXMEM_branchaddr; // branch address
+			end else if (PCEn && jump) begin
+				PC = IDEX_jumpaddr;
+			end else if (PCEn) begin
+				PC <= PC + 4;
+			end
+		end
+
+	// IFID pipeline register
+	always @(posedge clock or negedge reset)
+		begin 
+			if (~reset)
+				begin
+					IFID_PCplus4 <= 32'b0;
+					IFID_instr <= 32'hFF8F8F8F;
+				end
+			else if (IFID_En)
+				begin
+					IFID_PCplus4 <= PC + 32'd4;
+					IFID_instr <= instr;
+				end
+		end
+
+	Memory cpu_IMem(1'b1, 1'b1, 1'b1, 1'b0, PC >> 2, 32'b0, instr);
+
+	/***************** Instruction Decode Unit (ID) ****************/
+	assign imm = IFID_instr[15:0];
+	assign func = IFID_instr[5:0];
+	assign shamt = IFID_instr[10:6];
+	assign opcode = IFID_instr[31:26];
+	assign instr_rs = IFID_instr[25:21];
+	assign instr_rt = IFID_instr[20:16];
+	assign instr_rd = IFID_instr[15:11];
+	assign signExtend = {{16{imm[15]}}, imm};
+	assign jumpaddr = (IFID_instr[25:0] << 2) + IFID_PCplus4[31:28];
+
+	// Register file
+	RegFile cpu_regs(clock, reset, instr_rs, instr_rt, MEMWB_RegWriteAddr, MEMWB_RegWrite, wRegData, rdA, rdB);
+
+	// IDEX pipeline register
+	always @(posedge clock or negedge reset)
+		begin
+			if (~reset)
+				begin
+					IDEX_rdA <= 32'b0;
+					IDEX_rdB <= 32'b0;
+					IDEX_signExtend <= 32'b0;
+					IDEX_jumpaddr <= 32'b0;
+					IDEX_instr_rd <= 5'b0;
+					IDEX_instr_rs <= 5'b0;
+					IDEX_instr_rt <= 5'b0;
+					IDEX_RegDst <= 1'b0;
+					IDEX_ALUcntrl <= 2'b0;
+					IDEX_ALUSrc <= 1'b0;
+					IDEX_Branch <= 1'b0;
+					IDEX_MemRead <= 1'b0;
+					IDEX_MemWrite <= 1'b0;
+					IDEX_MemToReg <= 1'b0;
+					IDEX_RegWrite <= 1'b0;
+					IDEX_BneEn <= 1'b0;
+					IDEX_Shamt <= 1'b0;
+					IDEX_jump <= 1'b0;
+				end
+			else
+				begin
+					if (~bubble_idex || ((~BranchStall) && (PCSrc || jump)) )
+						begin
+							IDEX_ALUcntrl <= 0;
+							IDEX_ALUSrc <= 0;
+							IDEX_Branch <= 0;
+							IDEX_MemRead <= 0;
+							IDEX_MemWrite <= 0;
+							IDEX_MemToReg <= 0;
+							IDEX_RegWrite <= 0;
+							IDEX_BneEn <= 0;
+							IDEX_jump <= 0;
+						end
+					else
+						begin
+							IDEX_rdA <= rdA;
+							IDEX_rdB <= rdB;
+							IDEX_signExtend <= signExtend;
+							IDEX_jumpaddr <= jumpaddr;
+							IDEX_instr_rd <= instr_rd;
+							IDEX_instr_rs <= instr_rs;
+							IDEX_instr_rt <= instr_rt;
+							IDEX_RegDst <= RegDst;
+							IDEX_ALUcntrl <= ALUcntrl;
+							IDEX_ALUSrc <= ALUSrc;
+							IDEX_Branch <= Branch;
+							IDEX_MemRead <= MemRead;
+							IDEX_MemWrite <= MemWrite;
+							IDEX_MemToReg <= MemToReg;
+							IDEX_BneEn <= BneEn;
+							IDEX_RegWrite <= RegWrite;
+							IDEX_Shamt <= shamt;
+							IDEX_jump <= jump;
+						end
+				end
+		end
+
+	// Main Control Unit 
+	control_main control_main (RegDst, Branch, jump, MemRead, MemWrite, MemToReg, ALUSrc, RegWrite, BneEn, ALUcntrl, opcode);
+
+	hazard_unit hz_unit (PCEn, IFID_En, BranchStall, bubble_idex, IDEX_MemRead, IDEX_instr_rt, instr_rs, instr_rt, opcode);
+
+	assign rdBetween = (forwardB == 2'b00) ? IDEX_rdB : (forwardB == 2'b01) ? wRegData : EXMEM_ALUOut;
+
+	/***************** Execution Unit (EX) ****************/
+	assign ALUInA = (forwardA == 2'b00) ? IDEX_rdA : (forwardA == 2'b01) ? wRegData : EXMEM_ALUOut;
+	assign ALUInB = (IDEX_ALUSrc == 1'b1) ? IDEX_signExtend : rdBetween;
+
+	//  ALU
+	ALU cpu_alu(ALUOut, Zero, ALUInA, ALUInB, ALUOp, IDEX_Shamt);
+
+	assign RegWriteAddr = (IDEX_RegDst==1'b0) ? IDEX_instr_rt : IDEX_instr_rd;
+	assign branchaddr = (signExtend << 2) + 4;
+	assign PCSrc = Branch && (BneEn ? ~Zero : Zero);
+
+	// EXMEM pipeline register
+	always @(posedge clock or negedge reset)
+		begin
+			if (~reset)
+				begin
+					EXMEM_Zero <= 1'b0;
+					EXMEM_Branch <= 1'b0;
+					EXMEM_ALUOut <= 32'b0;
+					EXMEM_MemRead <= 1'b0;
+					EXMEM_MemWrite <= 1'b0;
+					EXMEM_MemToReg <= 1'b0;
+					EXMEM_RegWrite <= 1'b0;
+					EXMEM_RegWriteAddr <= 5'b0;
+					EXMEM_MemWriteData <= 32'b0;
+					EXMEM_branchaddr <= 32'b0;
+					EXMEM_PCSrc <= 1'b0;
+				end
+			else
+				begin
+					EXMEM_Zero <= Zero;
+					EXMEM_ALUOut <= ALUOut;
+					EXMEM_Branch <= IDEX_Branch;
+					EXMEM_branchaddr <= branchaddr;
+					EXMEM_MemRead <= IDEX_MemRead;
+					EXMEM_MemWriteData <= rdBetween; 
+					EXMEM_MemWrite <= IDEX_MemWrite;
+					EXMEM_MemToReg <= IDEX_MemToReg;
+					EXMEM_RegWrite <= IDEX_RegWrite;
+					EXMEM_RegWriteAddr <= RegWriteAddr;
+					EXMEM_PCSrc <= PCSrc;
+				end
+		end
+
+	// ALU control
+	control_alu control_alu(ALUOp, IDEX_ALUcntrl, IDEX_signExtend[5:0]);
+
+	forwarding_unit for_unit (forwardA, forwardB, forwardC, IDEX_instr_rs, IDEX_instr_rt, EXMEM_RegWriteAddr, MEMWB_RegWriteAddr, EXMEM_RegWrite, MEMWB_RegWrite, EXMEM_MemWrite);
+
+	/***************** Memory Unit (MEM) ****************/  
+	Memory cpu_DMem (clock, reset, EXMEM_MemRead, EXMEM_MemWrite, EXMEM_ALUOut, datatowrite, DMemOut);
+
+	// MEMWB pipeline register
+	always @(posedge clock or negedge reset)
+		begin
+			if (~reset)
+				begin
+					MEMWB_ALUOut <= 32'b0;
+					MEMWB_DMemOut <= 32'b0;
+					MEMWB_MemToReg <= 1'b0;
+					MEMWB_RegWrite <= 1'b0;
+					MEMWB_RegWriteAddr <= 5'b0;
+				end
+			else
+				begin
+					MEMWB_DMemOut <= DMemOut;
+					MEMWB_ALUOut <= EXMEM_ALUOut;
+					MEMWB_MemToReg <= EXMEM_MemToReg;
+					MEMWB_RegWrite <= EXMEM_RegWrite;
+					MEMWB_RegWriteAddr <= EXMEM_RegWriteAddr;
+				end
+		end
+
+	/***************** WriteBack Unit (WB) ****************/
+	assign wRegData = MEMWB_MemToReg ? MEMWB_DMemOut : MEMWB_ALUOut;
+	assign datatowrite = forwardC ? MEMWB_DMemOut : EXMEM_MemWriteData;
+endmodule
